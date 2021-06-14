8e41cdfa10ae7f7cc91044e022855c1184dac218
seunggiHong <tmdrl1026@naver.com>
2021-06-14 14:00:47 +0900

    baekjoon 9663 nqueens

--- main.py
+++ main.py
@@ -1,39 +1,23 @@
-#baekjoon 2580
-import sys
-
-sudoku = [list(map(int, sys.stdin.readline().split())) for i in range(9)]
-empty_space = [(i, j) for i in range(9) for j in range(9) if sudoku[i][j] == 0]
-
-def prommising(nx, ny, idx):
-    if idx in sudoku[nx]:
-        return False
-    
-    for i in range(9):
-        if sudoku[i][ny] == idx:
+def check(x):
+    for i in range(x):
+        if row[x] == row[i]:
             return False
-    
-    nx = nx // 3 * 3
-    ny = ny // 3 * 3
-    for i in range(3):
-        for j in range(3):
-            if sudoku[nx + i][ny + j] == idx:
-                return False 
-
+        if abs(row[x]- row[i]) == x - i:
+            return False
     return True
-
-def DFS(depth):
-    if depth == len(empty_space):
-        for i in sudoku:
-            for j in i:
-                print(j, end = ' ')
-            print()
-        sys.exit(0)
+    
+def dfs(x):
+    global result
+    if x == n:
+        result += 1
     else :
-        for i in range(1, 10):
-            nx = empty_space[depth][0]
-            ny = empty_space[depth][1]
-            if prommising(nx, ny, i):
-                sudoku[nx][ny] = i
-                DFS(depth + 1)
-                sudoku[nx][ny] = 0
-DFS(0)
+        for i in range(n):
+            row[x] = i
+            if check(x):
+                dfs(x + 1)
+
+n = int(input())
+row = [0] * n
+result = 0
+dfs(0)
+print(result)
