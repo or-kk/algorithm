1cb4a8cf0b7942fa0cd55b182518c1fe6a783b14
seunggiHong <tmdrl1026@naver.com>
2021-06-28 18:39:33 +0900

    baekjoon 11053

---
+++ dp/가장긴증가하는수열.py
@@ -0,0 +1,12 @@
+#baekjoon 11053
+n = int(input())
+ary = list(map(int, input().split()))
+
+dp = [1] * n 
+
+for i in range(n):
+    for j in range(i):
+        if ary[j] < ary[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
